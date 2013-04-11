1cfeb04abff8893b1f6831a7df3751f25d081521
Felipe Gonzalez <adonaac@gmail.com>
2013-04-11 01:12:25 -0300

    Add graph

--- tools/skill/Graph.lua
+++ tools/skill/Graph.lua
@@ -1,45 +1,57 @@
 require 'middleclass/middleclass'
 
 Graph = class('Graph')
-GraphNode = struct('name')
+GraphNode = struct('name', 'data')
+
+function tableToString(table)
+    local str = "{"
+    for k, v in pairs(table) do
+        if type(k) ~= "number" then str = str .. k .. " = " end
+        if type(v) == "number" or type(v) == "boolean" then str = str .. tostring(v) .. ", "
+        elseif type(v) == "string" then str = str .. "'" .. v .. "'" .. ", "
+        elseif type(v) == "table" then str = str .. tableToString(v) .. ", " end
+    end
+    if #table > 0 then str = string.sub(str, 1, -3) end
+    str = str .. "}"
+    return str
+end
 
 function Graph:initialize()
     self.graph = {}
-    self.nodes = {}
 end
 
 function Graph:addNode(node)
-    table.insert(self.nodes, node)
-    table.insert(self.graph, {})
+    self.graph[node.name] = {adjacent_to = {}, data = node.data}
 end
 
 function Graph:addEdge(node_name1, node_name2)
-    local i, j = self:getNodeIndexByName(node_name1), self:getNodeIndexByName(node_name2)
-    self.graph[i][j] = true
-    self.graph[j][i] = true
-end
-
-function Graph:getNode(name)
-    return self.nodes[self:getNodeIndexByName(name)]
-end
-
-function Graph:getNodeIndexByName(name)
-    for i, n in ipairs(self.nodes) do
-        if n.name == name then return i end
-    end
+    table.insert(self.graph[node_name1].adjacent_to, node_name2)
+    table.insert(self.graph[node_name2].adjacent_to, node_name1)
 end
 
 function Graph:removeNode(node_name)
-    local n = self:getNodeIndexByName(node_name)
-    table.remove(self.nodes, n)
-    for i = 1, #self.graph do table.remove(self.graph[i], n) end
-    table.remove(self.graph, n)
+    self.graph[node_name] = nil
+    for k, v in pairs(self.graph) do
+        for l, u in pairs(v.adjacent_to) do
+            if u == node_name then table.remove(v.adjacent_to, l) end
+        end
+    end
 end
 
 function Graph:removeEdge(node_name1, node_name2)
-    local i, j = self:getNodeIndexByName(node_name1), self:getNodeIndexByName(node_name2)
-    self.graph[i][j] = nil 
-    self.graph[j][i] = nil
+    for k, v in pairs(self.graph[node_name1].adjacent_to) do
+        if v == node_name2 then table.remove(self.graph[node_name1].adjacent_to, k) end
+    end
+
+    for k, v in pairs(self.graph[node_name2].adjacent_to) do
+        if v == node_name1 then table.remove(self.graph[node_name2].adjacent_to, k) end
+    end
 end
 
+function Graph:print()
+    for k, v in pairs(self.graph) do
+        print(k .. ": " .. tableToString(v.adjacent_to))
+    end
+    print()
+end
 
--- tools/skill/main.lua
+++ tools/skill/main.lua
@@ -1,6 +1,20 @@
 struct = require 'struct/struct'
 require 'Graph'
 
+
 function love.load()
     graph = Graph()
+    graph:addNode(GraphNode('A', 1))
+    graph:addNode(GraphNode('B', 1))
+    graph:addNode(GraphNode('C', 1))
+    graph:addNode(GraphNode('D', 1))
+    graph:print()
+    graph:addEdge('A', 'B')
+    graph:addEdge('B', 'C')
+    graph:addEdge('A', 'D')
+    graph:print()
+    graph:removeNode('C')
+    graph:print()
+    graph:removeEdge('A', 'D')
+    graph:print()
 end
