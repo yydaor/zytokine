bd6017445473168efb3271fb2b9e27c2ab35426c
Felipe Gonzalez <adonaac@gmail.com>
2013-04-12 00:14:27 -0300

    Add mouse exploring, dragging and basic selection

--- tools/skill/SkillTree.lua
+++ tools/skill/SkillTree.lua
@@ -1,7 +1,7 @@
 require 'Graph'
 
 SkillTree = class('SkillTree')
-SkillNode = struct('position', 'image', 'scale')
+SkillNode = struct('position', 'image', 'scale', 'selected')
 Node = struct('name', 'data')
 
 function SkillTree:initialize()
@@ -16,17 +16,35 @@ function SkillTree:addEdge(node_name1, node_name2)
     self.graph:addEdge(node_name1, node_name2)   
 end
 
-function SkillTree:update(dt)
-    
+function SkillTree:moveNodeTo(node_name, dx, dy)
+    self.graph.graph[node_name].data.position.x = self.graph.graph[node_name].data.position.x + dx
+    self.graph.graph[node_name].data.position.y = self.graph.graph[node_name].data.position.y + dy
 end
 
-function SkillTree:draw()
-    love.graphics.setLineWidth(1.5)
+function SkillTree:update(dt, m_x, m_y)
+    at_least_one_selected = false
     for k, v in pairs(self.graph.graph) do
         local x, y = v.data.position.x, v.data.position.y
         local w, h = v.data.image:getWidth(), v.data.image:getHeight()
         local scale = v.data.scale or 1
-        love.graphics.draw(v.data.image, x, y, 0, scale)
+        if m_x >= x and m_x <= x+w and m_y >= y and m_y <= y+h then
+            v.data.selected = true
+            at_least_one_selected = k
+        else v.data.selected = false end
     end
-    love.graphics.setLineWidth(1)
+end
+
+function SkillTree:draw()
+    for k, v in pairs(self.graph.graph) do
+        local x, y = v.data.position.x, v.data.position.y
+        local w, h = v.data.image:getWidth(), v.data.image:getHeight()
+        local scale = v.data.scale or 1
+        if v.data.selected then
+            love.graphics.setColorMode('combine')
+            love.graphics.setColor(240, 192, 240)
+            love.graphics.draw(v.data.image, x, y, 0, scale)
+            love.graphics.setColor(255, 255, 255)
+            love.graphics.setColorMode('replace')
+        else love.graphics.draw(v.data.image, x, y, 0, scale) end
+    end
 end
--- tools/skill/main.lua
+++ tools/skill/main.lua
@@ -2,29 +2,47 @@ struct = require 'struct/struct'
 Camera = require 'hump.camera'
 require 'utils'
 require 'SkillTree'
+require 'Vector'
 
 function loadImages()
-    background = love.graphics.newImage('background.png')
-    reflecting_image = love.graphics.newImage('reflecting.png')
+    background = love.graphics.newImage('resources/background.png')
+    instant = love.graphics.newImage('resources/instant.png')
+    dot = love.graphics.newImage('resources/dot.png')
 end
 
 
 function love.load()
     loadImages()
+
     width = love.graphics.getWidth() 
     height = love.graphics.getHeight()
     camera = Camera(width/2, height/2)
+
     skill_tree = SkillTree()
-    last_mouse = {x = 0, y = 0}
+    skill_tree:addNode(Node('Instant', SkillNode(Vector(100, 100), instant, 1)))
+
+    last_mouse = Vector() 
+    current_mouse = Vector() 
+
+    -- Logic controls
+    at_least_one_selected = false
 end
 
 function love.update(dt)
-    skill_tree:update(dt)
+    -- Move around the tree
+    last_mouse.x, last_mouse.y = current_mouse.x, current_mouse.y
+    current_mouse.x, current_mouse.y = love.mouse.getPosition()
+    local d = current_mouse - last_mouse
 
-    last_mouse.x, last_mouse.y = love.mouse.getPosition()
     if love.mouse.isDown('l') then
-        
+        if not at_least_one_selected then
+            camera:move(-d.x*(1/camera.scale), -d.y*(1/camera.scale))
+        else 
+            skill_tree:moveNodeTo(at_least_one_selected, d.x*(1/camera.scale), d.y*(1/camera.scale))
+        end
     end
+
+    skill_tree:update(dt, camera:mousepos())
 end
 
 function love.draw()
@@ -33,3 +51,15 @@ function love.draw()
     skill_tree:draw()
     camera:detach()
 end
+
+function love.mousepressed(x, y, button)
+    -- Zoom in
+    if button == 'wu' then 
+        if camera.scale < 2 then camera:zoom(1.2) end
+
+    -- Zoom out
+    elseif button == 'wd' then 
+        if camera.scale > 0.5 then camera:zoom(0.8) end
+    end
+end
+
